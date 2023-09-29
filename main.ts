9bc924756e502c3cd0a5a1e8a423a05341265b00
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-09-29 13:03:37 -0400

    typescript change 3

--- main.ts
+++ main.ts
@@ -7,15 +7,15 @@
 
 // setup
 basic.clearScreen()
-  pins.digitalWritePin(DigitalPin.P16,0)
-  basic.showIcon(IconNames.Happy)
+pins.digitalWritePin(DigitalPin.P16, 0)
+basic.showIcon(IconNames.Happy)
 
-  // turn on LED
+// turn on LED
 input.onButtonPressed(Button.A, function () {
   basic.showIcon(IconNames.Yes)
   pins.digitalWritePin(DigitalPin.P16, 1)
 })
-  // turn off LED
+// turn off LED
 input.onButtonPressed(Button.A, function () {
   basic.showIcon(IconNames.No)
   pins.digitalWritePin(DigitalPin.P16, 0)
